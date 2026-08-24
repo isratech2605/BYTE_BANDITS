a7eb7a7e966fd34d6a4339e7d6142460df8f9e96
Israatech <isratech2605@gmail.com>
2026-08-24 19:20:38 +0530

    Create initial Streamlit app

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="ProcureTrace")
+
+st.title("🔎 ProcureTrace")
+st.subheader("Procurement Risk Intelligence")
+
+st.write(
+    "A prototype for identifying high-risk financial patterns "
+    "across procurement transactions."
+)
+
+st.success("Prototype setup successful! 🚀")
